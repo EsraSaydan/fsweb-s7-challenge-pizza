5e090957d8cdcf2a8f2a23e481ffc9d6d798f121
EsraSaydan <esracarturk.77@gmail.com>
2024-06-16 19:39:23 +0300

    data create

---
+++ src/data/data.js
@@ -0,0 +1,182 @@
+import food1 from "/Assets/mile2-aseets/pictures/food-1.png";
+import food2 from "/Assets/mile2-aseets/pictures/food-2.png";
+import food3 from "/Assets/mile2-aseets/pictures/food-3.png";
+import ramen1 from "/Assets/ramen/ramen1.jpg";
+import ramen2 from "/Assets/ramen/ramen2.jpg";
+
+export const data = [
+  {
+    id: 1,
+    name: "Shoyu",
+    category: "ramen",
+    image: ramen1,
+    like: "4.9",
+    comment: "(200)",
+    price: "60₺",
+  },
+  {
+    id: 2,
+    name: "Miso",
+    category: "ramen",
+    image: ramen2,
+    like: "4.9",
+    comment: "(200)",
+    price: "60₺",
+  },
+  {
+    id: 3,
+    name: "Tonkotsu",
+    category: "ramen",
+    image: ramen1,
+    like: "4.9",
+    comment: "(200)",
+    price: "60₺",
+  },
+  {
+    id: 4,
+    name: "Terminal Pizza",
+    category: "pizza",
+    image: food1,
+    like: "4.9",
+    comment: "(200)",
+    price: "60₺",
+  },
+  {
+    id: 5,
+    name: "Position Absolute Acı Pizza",
+    category: "pizza",
+    image: food2,
+    like: "4.9",
+    comment: "(200)",
+    price: "85₺",
+  },
+  {
+    id: 6,
+    name: "useEffect Tavuklu Pizza",
+    category: "pizza",
+    image: food3,
+    like: "4.9",
+    comment: "(200)",
+    price: "75₺",
+  },
+  {
+    id: 7,
+    name: "Double Cheeseburger",
+    category: "burger",
+    image:
+      "https://cdn.pixabay.com/photo/2022/05/25/21/28/burger-7221436_1280.jpg",
+    like: "4.9",
+    comment: "(200)",
+    price: "100₺",
+  },
+  {
+    id: 8,
+    name: "Bacon Cheeseburger",
+    category: "burger",
+    image:
+      "https://images.unsplash.com/photo-1553979459-d2229ba7433b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60",
+    like: "4.9",
+    comment: "(200)",
+    price: "120₺",
+  },
+  {
+    id: 9,
+    name: "Mushroom Burger",
+    category: "burger",
+    image:
+      "https://cdn.pixabay.com/photo/2022/07/15/18/12/mushroom-burger-7323675_1280.jpg",
+    like: "4.9",
+    comment: "(200)",
+    price: "125₺",
+  },
+  {
+    id: 10,
+    name: "Bol Bol atıştır!",
+    category: "French fries",
+    image:
+      "https://cdn.pixabay.com/photo/2014/01/16/01/48/chicken-nuggets-246180_1280.jpg",
+    like: "4.9",
+    comment: "(200)",
+    price: "90₺",
+  },
+  {
+    id: 11,
+    name: "Chedar soslu patates",
+    category: "French fries",
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/21/15/52/french-fries-1846083_1280.jpg",
+    like: "4.9",
+    comment: "(200)",
+    price: "95₺",
+  },
+  {
+    id: 12,
+    name: "Bol soslu patates",
+    category: "French fries",
+    image:
+      "https://cdn.pixabay.com/photo/2020/03/28/14/51/french-fries-4977353_1280.jpg",
+    like: "4.9",
+    comment: "(200)",
+    price: "85₺",
+  },
+  {
+    id: 13,
+    name: "useEffect Soslu Chicken",
+    category: "Fast food",
+    image:
+      "https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    like: "4.9",
+    comment: "(200)",
+    price: "110₺",
+  },
+  {
+    id: 14,
+    name: "Baked Chicken",
+    category: "Fast food",
+    image:
+      "https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    like: "4.9",
+    comment: "(200)",
+    price: "120₺",
+  },
+  {
+    id: 15,
+    name: "Chicken Tenders",
+    category: "Fast food",
+    image:
+      "https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    like: "4.9",
+    comment: "(200)",
+    price: "100₺",
+  },
+  {
+    id: 16,
+    name: "Ice Mocha",
+    category: "Soft drinks",
+    image:
+      "https://cdn.pixabay.com/photo/2020/03/07/05/17/coffee-4908762_1280.jpg",
+    like: "4.9",
+    comment: "(200)",
+    price: "85₺",
+  },
+  {
+    id: 17,
+    name: "Limonata",
+    category: "Soft drinks",
+    image:
+      "https://cdn.pixabay.com/photo/2017/02/25/09/30/lemonade-2097312_1280.jpg",
+    like: "4.9",
+    comment: "(200)",
+    price: "70₺",
+  },
+  {
+    id: 18,
+    name: "Cocktail",
+    category: "Soft drinks",
+    image:
+      "https://cdn.pixabay.com/photo/2019/08/21/15/24/cocktail-4421316_1280.jpg",
+    like: "4.9",
+    comment: "(200)",
+    price: "90₺",
+  },
+];
