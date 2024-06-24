f6b827a2a3145362887478594c0759c492ad6d59
EsraSaydan <esracarturk.77@gmail.com>
2024-06-24 13:36:48 +0300

    css done

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -15,21 +15,21 @@ export default function Success({ currentOrder }) {
   console.log(currentOrder);
 
   return (
-    <div className="flex flex-col justify-start items-center  bg-customRed">
+    <div className="h-[100vh] flex flex-col justify-start items-center  bg-customRed">
       <h3 className="font-satisfy text-2xl text-customYellow mt-[30px]">
         Lezzetin Yolda
       </h3>
-      <div className="font-barlow text-customBej text-4xl lg:text-7xl mt-4">
+      <div className="font-barlow text-customBej text-4xl lg:text-6xl mt-4">
         <h1> SİPARİŞ ALINDI</h1>
-        <hr className="text-white mt-5 font-bold" />
+        <hr className="text-white mt-3 font-bold" />
       </div>
       <p className="text-customBej mt-4 text-2xl">
         Position Absolute Acı Pizza
       </p>
 
-      <hr className="text-white mt-5 font-bold" />
+      <hr className="text-white font-bold" />
 
-      <div className="text-customBej px-25 mr-8 bg-customRed">
+      <div className="text-customBej px-25 mr-8 bg-customRed mb-[1rem]">
         <br />
         <p className="mt-5 ">Boyut: {currentOrder.boyutSec}</p>
         <br />
@@ -38,7 +38,7 @@ export default function Success({ currentOrder }) {
         <p>Ek Malzemeler: {formatEkMalzemeler(currentOrder.ekMalzeme)}</p>
       </div>
 
-      <div className="mt-[90px] border-1 p-10 rounded-2  text-customBej px-20  bg-customRed mb-[100px]">
+      <div className="gap-0 border-1 p-10 rounded-2  text-customBej mt-[3rem] px-20  bg-customRed ">
         <h4 className="mb-4 text-xl">Sipariş Toplamı</h4>
         <p className="mb-2">Seçimler: {currentOrder.totalMalzemePrice}₺</p>
         <p className="toplam-success">Toplam: {currentOrder.toplamUcret}₺</p>
